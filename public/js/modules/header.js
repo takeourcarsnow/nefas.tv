bda88566856511bb28a435e67014037206c51bf9
takeourcarsnow <ignasnefas@gmail.com>
2025-08-02 17:40:01 +0300

    Delete public directory

--- public/js/modules/header.js
+++
@@ -1,9 +0,0 @@
-export const headerFrames = ['[><]', '[> <]', '[><]', '[>  <]'];
-
-export const initHeaderAnimation = (asciiHeader) => {
-    let headerFrameIndex = 0;
-    setInterval(() => {
-        if (asciiHeader) asciiHeader.textContent = headerFrames[headerFrameIndex];
-        headerFrameIndex = (headerFrameIndex + 1) % headerFrames.length;
-    }, 500);
-};
